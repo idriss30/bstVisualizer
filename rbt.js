6ac17f5e829b71b69552fc3bb8c230af66c750d7
idriss30 <idriss.cissoko@hotmail.com>
2024-09-01 22:49:40 -0400

    rbt node class created

--- rbt.js
+++ rbt.js
@@ -3,3 +3,32 @@ implementation of a Red black Tree with different methods for searching, inserti
 deleting, Depth-first search.
 
 */
+
+// single node class
+class RbtNode {
+  constructor(key) {
+    this.key = key;
+    this.parent = null;
+    this.leftNode = null;
+    this.rightNode = null;
+    this.isBlack = false;
+  }
+}
+
+class RbtTree {
+  constructor() {
+    this.size = 0;
+    this.root = null;
+  }
+
+  // insert
+
+  insert(key) {
+    if (!this.size) {
+      const node = new RbtNode(key);
+      node.isBlack = true;
+      this.root = node;
+      return true;
+    }
+  }
+}
